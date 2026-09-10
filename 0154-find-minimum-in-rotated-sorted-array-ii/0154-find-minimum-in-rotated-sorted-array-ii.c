c0c5af62bdf2588caafe34e7d06d48a25ee35fe1
anithaanitha7803 <anithaanitha7803@gmail.com>
2026-09-10 12:31:57 +0530

    Time: 0 ms (100%), Space: 9.1 MB (84.76%) - LeetHub

---
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.c
@@ -0,0 +1,11 @@
+int findMin(int* nums, int numsSize) {
+    int min=nums[0];
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]<min)
+        {
+            min=nums[i];
+        }
+    }
+    return min;
+}
