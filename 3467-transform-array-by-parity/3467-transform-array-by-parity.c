efa16feae8ba5e4604ff18b49c4500b7fc2cba9a
anithaanitha7803 <anithaanitha7803@gmail.com>
2026-09-10 12:50:40 +0530

    Time: 0 ms (100%), Space: 12.6 MB (56%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.c
@@ -0,0 +1,24 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* transformArray(int* nums, int numsSize, int* returnSize) {
+    int *ans=malloc(numsSize*sizeof(int));
+    *returnSize=numsSize;
+    int even=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]%2==0)
+        {
+            even++;
+        }
+    }
+    for(int i=0;i<even;i++)
+    {
+        ans[i]=0;
+    }
+    for(int i=even;i<numsSize;i++)
+    {
+        ans[i]=1;
+    }
+    return ans;
+}
